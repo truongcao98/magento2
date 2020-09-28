4b70efdacd6933b36f0a99a8b92f25ee5c2047d0
truongcao98 <kutruong05031998@gmail.com>
2020-09-28 23:34:53 +0700

    Create Info.php

---
+++ app/code/Truongcl/Hello/Block/Adminhtml/Posts/Edit/Tab/Info.php
@@ -0,0 +1,148 @@
+<?php
+namespace Truongcl\Hello\Block\Adminhtml\Posts\Edit\Tab;
+
+use Magento\Backend\Block\Widget\Form\Generic;
+use Magento\Backend\Block\Widget\Tab\TabInterface;
+use Magento\Backend\Block\Template\Context;
+use Magento\Framework\Registry;
+use Magento\Framework\Data\FormFactory;
+use Magento\Cms\Model\Wysiwyg\Config;
+use Truongcl\Hello\Model\System\Config\Status;
+
+class Info extends Generic implements TabInterface
+{
+    /**
+     * @var \Magento\Cms\Model\Wysiwyg\Config
+     */
+    protected $_wysiwygConfig;
+
+    /**
+     * @var \Truongcl\Hello\Model\System\Config\Status
+     */
+    protected $_status;
+    /**
+     * @param Context $context
+     * @param Registry $registry
+     * @param FormFactory $formFactory
+     * @param Config $wysiwygConfig
+     * @param Status $status
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        Registry $registry,
+        FormFactory $formFactory,
+        Config $wysiwygConfig,
+        Status $status,
+        array $data = []
+    ) {
+        $this->_wysiwygConfig = $wysiwygConfig;
+        $this->_status = $status;
+        parent::__construct($context, $registry, $formFactory, $data);
+    }
+
+    /**
+     * Prepare form fields
+     *
+     * @return \Truongcl\Backend\Block\Widget\Form
+     */
+    protected function _prepareForm()
+    {
+        /** @var $model \Truongcl\Hello\Model\PostsFactory */
+        $model = $this->_coreRegistry->registry('Truongcl_blog');
+
+        /** @var \Truongcl\Framework\Data\Form $form */
+        $form = $this->_formFactory->create();
+        $form->setHtmlIdPrefix('post_');
+        $form->setFieldNameSuffix('post');
+        // new filed
+
+        $fieldset = $form->addFieldset(
+            'base_fieldset',
+            ['legend' => __('General')]
+        );
+
+        if ($model->getId()) {
+            $fieldset->addField(
+                'id',
+                'hidden',
+                ['name' => 'id']
+            );
+        }
+        $fieldset->addField(
+            'title',
+            'text',
+            [
+                'name'      => 'title',
+                'label'     => __('Title'),
+                'title' => __('Title'),
+                'required' => true
+            ]
+        );
+        $fieldset->addField(
+            'image',
+            'text',
+            array(
+                'name'      => 'image',
+                'label'     => __('Image'),
+                'title' => __('Image'),
+            )
+        );
+        $fieldset->addField(
+            'status',
+            'select',
+            [
+                'name'      => 'status',
+                'label'     => __('Status'),
+                'options'   => $this->_status->toOptionArray()
+            ]
+        );
+        $fieldset->addField('description', 'editor', [
+            'name'      => 'description',
+            'label'   => 'Description',
+            'config'    => $this->_wysiwygConfig->getConfig(),
+            'wysiwyg'   => true,
+            'required'  => false
+        ]);
+        $data = $model->getData();
+        $form->setValues($data);
+        $this->setForm($form);
+
+        return parent::_prepareForm();
+    }
+    /**
+     * Prepare label for tab
+     *
+     * @return string
+     */
+    public function getTabLabel()
+    {
+        return __('Posts Info');
+    }
+
+    /**
+     * Prepare title for tab
+     *
+     * @return string
+     */
+    public function getTabTitle()
+    {
+        return __('Posts Info');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function canShowTab()
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isHidden()
+    {
+        return false;
+    }
+}
