b09e132bb5ed92d2089c17bcca656546b48ac7be
truongcao98 <kutruong05031998@gmail.com>
2020-09-28 23:35:26 +0700

    Create Tabs.php

---
+++ app/code/Truongcl/Hello/Block/Adminhtml/Posts/Edit/Tabs.php
@@ -0,0 +1,40 @@
+<?php
+namespace Truongcl\Hello\Block\Adminhtml\Posts\Edit;
+
+use Magento\Backend\Block\Widget\Tabs as WidgetTabs;
+
+class Tabs extends WidgetTabs
+{
+    /**
+     * Class constructor
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        parent::_construct();
+        $this->setId('post_edit_tabs');
+        $this->setDestElementId('edit_form');
+        $this->setTitle(__('Post Information'));
+    }
+
+    /**
+     * @return $this
+     */
+    protected function _beforeToHtml()
+    {
+        $this->addTab(
+            'post_info',
+            [
+                'label' => __('General'),
+                'title' => __('General'),
+                'content' => $this->getLayout()->createBlock(
+                    'Truongcl\Hello\Block\Adminhtml\Posts\Edit\Tab\Info'
+                )->toHtml(),
+                'active' => true
+            ]
+        );
+
+        return parent::_beforeToHtml();
+    }
+}
